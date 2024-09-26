6fc924753afc2058096eddd3ba6693c9ac0a5af9
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-26 14:35:45 +0300

    Adding some settings.

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEVELOPMENT_DEBUG', default=True, cast=bool)
+
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ecomstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
