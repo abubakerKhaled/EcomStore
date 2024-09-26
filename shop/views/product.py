81d807905f718080c5a893049d48595ec2b822dc
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-26 18:03:18 +0300

    Update the product list view.

---
+++ shop/views/product.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from ..models.product import Product
+from ..models.category import Category
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'shop/product/list.html', context)
+
