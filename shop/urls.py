f59a365f56edbf7bc1efac17c81e62a409cc4061
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-26 18:27:12 +0300

    Adding more paths to the urls.

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,14 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    
+    path(
+        '<slug:category_slug>/',
+        views.product_list,
+        name='product_list_by_category'
+    ),
+    path(
+        '<uuid:uuid>/<slug:slug>/',
+        views.product_details,
+        name='product_details'
+    )
 ]
