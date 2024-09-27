729b4ffd314d05fd9ab4d398b3e1204a363ac245
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-27 15:45:52 +0300

    Update the cart class.

--- cart/cart.py
+++ cart/cart.py
@@ -35,6 +35,14 @@ class Cart:
             item['total_price'] = item['price'] * item['quantity']
             yield item
 
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+
     def add(self, product, quantity=1, override_quantity=False):
         """
         Add a product to the cart or update its quantity.
@@ -69,3 +77,14 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+    
+    def get_total_price(self):
+        return sum(
+            Decimal(item['price']) * item['quantity']
+            for item in self.cart.values() 
+        )
+    
+    def clear(self):
+        # Remove the cart from session
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
