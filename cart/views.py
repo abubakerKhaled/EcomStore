7d606eb881e08081e2653e56ac3c9ed3da368490
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-28 10:45:21 +0300

    Update the views file.

--- cart/views.py
+++ cart/views.py
@@ -3,13 +3,14 @@ from django.views.decorators.http import require_POST
 from shop.models.product_model import Product
 from .cart import Cart
 from .forms import CartAddProductForm
-
+from django.contrib import messages  # Add messages for feedback
 
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
+    product = get_object_or_404(Product, uuid=product_id)
     form = CartAddProductForm(request.POST)
+    
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(
@@ -23,9 +24,10 @@ def cart_add(request, product_id):
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
+    product = get_object_or_404(Product, uuid=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
+
 
 def cart_detail(request):
     cart = Cart(request)
