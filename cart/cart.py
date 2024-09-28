cda5a190d77e581bb0c66bfbd68d40c2e099ac56
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-28 11:08:38 +0300

    Update urls and html files.

--- cart/cart.py
+++ cart/cart.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models.product_model import Product
-
+from uuid import UUID
 
 class Cart:
     def __init__(self, request):
@@ -16,14 +16,14 @@ class Cart:
 
     def __iter__(self):
         """
-        Iterate over the items in the cart and get the products from the database.
+        Iterate over the items in the cart and get the products
+        from the database using UUID.
         """
-        product_uuids = self.cart.keys()  # Get the UUIDs from the cart
-        products = Product.objects.filter(uuid__in=product_uuids)  # Query using UUIDs
-
+        product_uuids = self.cart.keys()
+        products = Product.objects.filter(uuid__in=product_uuids)
         cart = self.cart.copy()
         for product in products:
-            cart[str(product.uuid)]['product'] = product  # Use UUID as the key
+            cart[str(product.uuid)]['product'] = product
         for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
@@ -37,9 +37,9 @@ class Cart:
 
     def add(self, product, quantity=1, override_quantity=False):
         """
-        Add a product to the cart or update its quantity.
+        Add a product to the cart or update its quantity, using the UUID.
         """
-        product_uuid = str(product.uuid)  # Use UUID as the key
+        product_uuid = str(product.uuid)
         if product_uuid not in self.cart:
             self.cart[product_uuid] = {
                 'quantity': 0,
@@ -52,14 +52,11 @@ class Cart:
         self.save()
 
     def save(self):
-        """
-        Mark the session as modified to ensure it gets saved.
-        """
         self.session.modified = True
 
     def remove(self, product):
         """
-        Remove a product from the cart.
+        Remove the product from the cart using UUID.
         """
         product_uuid = str(product.uuid)
         if product_uuid in self.cart:
@@ -67,17 +64,12 @@ class Cart:
             self.save()
 
     def get_total_price(self):
-        """
-        Get the total price of items in the cart.
-        """
         return sum(
             Decimal(item['price']) * item['quantity']
             for item in self.cart.values()
         )
 
     def clear(self):
-        """
-        Remove the cart from the session.
-        """
+        # Remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
