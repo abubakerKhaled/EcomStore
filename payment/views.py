357fd38cacae077ed7c1d6e7d55d80b579d1bf70
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-10-02 22:44:36 +0300

    the completed and canceled views.

--- payment/views.py
+++ payment/views.py
@@ -54,3 +54,11 @@ def payment_process(request):
         return render(
             request, 'payment/process.html', locals()
         )
+    
+
+def payment_completed(request):
+    return render(request, 'payment/completed.html')
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
+
