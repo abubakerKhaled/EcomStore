b09a2c86d3670a3cac59ad2ed4a9941805788d78
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-10-02 22:40:45 +0300

    adding the line items of the session data.

--- payment/views.py
+++ payment/views.py
@@ -30,6 +30,21 @@ def payment_process(request):
             'line_items': []
         }
 
+        # add order items to the Stripe checkout session
+        for item in order.items.all():
+            session_data['line_items'].append(
+                {
+                    'price_data': {
+                        'unit_amount': int(item.price * Decimal('100')),
+                        'currency': 'egp',
+                        'product_data': {
+                            'name': item.product.name,
+                        },
+                    },
+                    'quantity': item.quantity,
+                }
+            )
+
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         # redirect to Stripe payment form
