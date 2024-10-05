b5a1b6679dccc80c60a4f1bf00d6b02a80b26af1
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-10-05 14:16:45 +0300

    Add the generate pdf view.

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,12 @@ from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem, Order
 from .tasks import order_created
+import weasyprint
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+
 
 def order_create(request):
     cart = Cart(request)
@@ -40,6 +46,24 @@ def order_create(request):
             context 
         )
     
+
+
+
+## View to generate the pdf
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order_id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheet=[weasyprint.CSS(finders.find('css/pdf.css'))],
+    )
+
+
+
+
 @staff_member_required
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
