caec042cc728d98c3b95fa6265934133098ea126
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-10-17 14:01:32 +0300

    add translation to the urls.

--- payment/urls.py
+++ payment/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views, webhooks
+from django.utils.translation import gettext_lazy as _
 
 app_name = 'payment'
 
 urlpatterns = [
-    path('process/', views.payment_process, name='process'),
-    path('completed/', views.payment_completed, name='completed'),
-    path('canceled/', views.payment_canceled, name='canceled'),
-    path('webhook/', webhooks.stripe_webhook, name='stripe-webhook'),
+    path(_("process/"), views.payment_process, name="process"),
+    path(_("completed/"), views.payment_completed, name="completed"),
+    path(_("canceled/"), views.payment_canceled, name="canceled"),
+    # we will remove the webhook url because we need a single URL for Stripe to notify events.
+    # and we need to avoid language prefixes in the webhook URL.
+    # path("webhook/", webhooks.stripe_webhook, name="stripe-webhook"),
 ]
