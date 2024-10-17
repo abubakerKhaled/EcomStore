ff7edb999f112bfaa71f1f8cdc96a27c7cb47718
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-10-17 15:35:01 +0300

    make migrations to the models.

--- shop/models/category_model.py
+++ shop/models/category_model.py
@@ -1,27 +1,28 @@
 from django.db import models
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 import uuid
 
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+
+class Category(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(max_length=200),
+        slug=models.SlugField(max_length=200, unique=True),  # Added the missing comma
+    )
     uuid = models.UUIDField(
-        default=uuid.uuid4,
-        editable=False,
-        unique=True
+        default=uuid.uuid4, editable=False, unique=True
     )  # UUID as a secondary unique identifier
-    
+
     class Meta:
-        ordering = ['name']
-        indexes = [
-            models.Index(fields=['name']),
-        ]
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
-        
+        # ordering = ["name"]
+        # indexes = [
+        #     models.Index(fields=["name"]),
+        # ]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse("shop:product_list_by_category", args=[self.slug])
-    
--- shop/models/product_model.py
+++ shop/models/product_model.py
@@ -1,42 +1,41 @@
 from django.db import models
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 from .category_model import Category
 import uuid
-class Product(models.Model):
+
+
+class Product(TranslatableModel):
     category = models.ForeignKey(
-        Category,
-        related_name = 'products',
-        on_delete= models.CASCADE
+        Category, related_name="products", on_delete=models.CASCADE
     )
     uuid = models.UUIDField(
-        default=uuid.uuid4,
-        editable=False, 
-        unique=True
+        default=uuid.uuid4, editable=False, unique=True
     )  # UUID as secondary identifier
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
-    image = models.ImageField(
-        upload_to = 'products/%Y/%m/%d',
-        blank=True
+
+    translations = TranslatedFields(
+        name=models.CharField(max_length=200),
+        slug=models.SlugField(max_length=200),
+        description=models.TextField(blank=True),
     )
-    description = models.TextField(blank=True)
+
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['name']
+        # ordering = ["name"]
         indexes = [
-            models.Index(fields=['id', 'slug']),
-            models.Index(fields=['uuid', 'slug']),
-            models.Index(fields=['name']),
-            models.Index(fields=['-created']),
+            # models.Index(fields=["id", "slug"]),
+            # models.Index(fields=["uuid", "slug"]),
+            # models.Index(fields=["name"]),
+            models.Index(fields=["-created"]),
         ]
-        
+
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse("shop:product_detail", args=[self.uuid, self.slug])
-    
